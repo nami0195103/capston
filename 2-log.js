c5236ac85948f3ba89d90714ea44a08020751e75
nami0195103 <48424503+nami0195103@users.noreply.github.com>
2019-03-31 17:06:02 +0900

    draw temperature graph using Google line chart

--- 2-log.js
+++ 2-log.js
@@ -25,6 +25,38 @@ function insert_sensor(device, unit, type, value, seq, ip) {
         });
 }
 
+app.get('/graph', function (req, res) {
+    console.log('got app.get(graph)');
+    var html = fs.readFile('./graph.html', function (err, html) {
+    html = " "+ html
+    console.log('read file');
+
+    var qstr = 'select * from sensors ';
+    connection.query(qstr, function(err, rows, cols) {
+      if (err) throw err;
+
+      var data = "";
+      var comma = ""
+      var start = String(rows[0].time)  //saves when data starts
+      for (var i=0; i< rows.length; i++) {
+         r = rows[i];
+         var time = String(r.time);
+         data += comma + "[new Date(2019,02," + time.substring(8, 10) + "," + time.substring(16, 18) + "," + time.substring(19, 21) + "," + time.substring(22, 24) + ")," + r.value +"]"
+         comma = ",";
+      }
+      var header = "data.addColumn('date', 'Date/Time');"
+      header += "data.addColumn('number', 'Temp');"
+      html = html.replace("<%HEADER%>", header);
+      html = html.replace("<%DATA%>", data);
+
+      res.writeHeader(200, {"Content-Type": "text/html"});
+      res.write(html);
+      res.write('data starts at ' + start + '   , ends at ' + time)
+      res.end();
+    });
+  });
+})
+
 app.get('/dump', function (req, res) {  //send saved data to dump page
         var cnt = req.query.count;
         var query = connection.query('select * from sensors order by time desc limit ?', Number(cnt), function(err, rows) {
